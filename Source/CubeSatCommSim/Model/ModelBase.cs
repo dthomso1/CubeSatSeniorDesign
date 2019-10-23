3ecef26cfe684f3e66f650af1de93599f47fa5c3
Thomas MacGregor <tommac@bellaliant.net>
2019-10-23 14:00:42 -0300

    Added base model and view model classes

---
+++ Source/CubeSatCommSim/Model/ModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace VSQLExtractor.Model
+{
+    public class ModelBase
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged(string prop)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ Source/CubeSatCommSim/ViewModel/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace VSQLExtractor.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+    }
+}
