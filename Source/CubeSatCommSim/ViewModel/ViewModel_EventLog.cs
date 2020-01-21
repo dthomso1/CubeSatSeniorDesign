f821c19cd18e26c3f3e25dab34d2c09515cc9e0f
Maurice <mlavoie1@unb.ca>
2020-01-21 15:40:32 -0400

    Added Clear Log and ICommand classes

--- Source/CubeSatCommSim/ViewModel/ViewModel_EventLog.cs
+++ Source/CubeSatCommSim/ViewModel/ViewModel_EventLog.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using CubeSatCommSim.Model;
 using CubeSatCommSim.View;
+using System.Windows.Input;
 
 namespace CubeSatCommSim.ViewModel
 {
@@ -19,6 +20,21 @@ namespace CubeSatCommSim.ViewModel
             this.vw = vw;
             md = new EventLog();
             vw.DataContext = EventList;
+        }
+
+        public void ClearLog_vm() => md.ClearLog();
+
+        RelayCommand _clearCommand;
+        public ICommand ClearCommand
+        {
+            get
+            {
+                if (_clearCommand == null)
+                {
+                    _clearCommand = new RelayCommand(param => this.ClearLog_vm());
+                }
+                return _clearCommand;
+            }
         }
     }
 }
