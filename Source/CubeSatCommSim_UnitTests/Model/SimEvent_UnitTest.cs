6a982ee9498533192e994d0bf41ff5bb56ec6a26
Alex <arostant@unb.ca>
2020-02-23 09:51:57 -0400

    EventLog and SimEvent unit tests

--- Source/CubeSatCommSim_UnitTests/Model/SimEvent_UnitTest.cs
+++ Source/CubeSatCommSim_UnitTests/Model/SimEvent_UnitTest.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CubeSatCommSim.Model;
 
 namespace CubeSatCommSim_UnitTestss
 {
     [TestClass]
-    public class BusTest
+    public class SimEvent_UnitTest
     {
         [TestMethod]
-        public void _()
+        public void SimEvent_Test_AddingInfo_ToString()
         {
+            String log = "Test log";
+            EventSeverity severity = new EventSeverity();
+            severity.set(SimEvent.INFO);
+            SimEvent testEvent = new SimEvent(log, severity);
+            Assert.areEquals("[INFO] Test log", testEvent.toString());
 
         }
     }
